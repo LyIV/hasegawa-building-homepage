f4b246b94e09ca2231c0a119165372984f4f9c6f
0x04 <0x04@example.com>
2021-10-10 23:39:06 +0900

    style: add screen config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@ module.exports = {
   purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    screens: {
+        'sm': {'max': '767px'},
+        'md': {'min': '768px' , 'max': '1023px'},
+        'lg': {'min': '1024px', 'max': '1279px'},
+        'xl': {'min': '1280px'}
+    }
   },
   variants: {
     extend: {},
